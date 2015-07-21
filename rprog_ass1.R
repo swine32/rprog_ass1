da00f15f8ef8b642cf66bd6d7ce75aabd7b4cb8a
swine32 <nswinehart@uchicago.edu>
2015-07-21 16:05:08 -0500

    added rprog_ass1.R

---
+++ rprog_ass1.R
@@ -0,0 +1,9 @@
+pollutantmean <- function(directory, pollutant, id) {
+    setwd(directory)
+    read.csv(id)
+    sub <- subset(id$pollutant
+    
+}
+
+pollutantmean("/Users/Swinehart/Downloads/specdata", 
+              "nitrate", "001.csv")
